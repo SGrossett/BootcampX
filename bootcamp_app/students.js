661646d427d5d8bf544c3810ff5ad1b9880802c8
Selene Grossett <selene_816@hotmail.com>
2022-01-05 17:18:49 +0000

    Update function to allow user to specify and cohort name and limit from the command line.

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,12 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const args = process.argv.slice(2);
+const cohortName = args[0];
+const limit = args[1];
+
+const inserts = [`%${cohortName}%`, limit]
+
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-  FROM students
-  JOIN cohorts ON cohort_id = cohorts.id
-  LIMIT 5;
-`)
+FROM students
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, inserts)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
