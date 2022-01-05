507abcb5bb0c978f03bcb94f1e402da3cb41f63f
Selene Grossett <selene_816@hotmail.com>
2022-01-05 17:33:05 +0000

    Run a query using node-postgres to find the name of all the teachers that made an assistance request during a certain cohort.

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,27 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = process.argv[2];
+const inserts = [`%${cohortName || 'JUL02'}%`]
+
+pool.query(`
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name LIKE $1
+  ORDER BY teacher
+`, inserts)
+.then(res => {
+  console.log('connected');
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+});
